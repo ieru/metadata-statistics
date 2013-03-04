5fd1f1d394a70610ae5bbb1c74a2167eec0a5b7c
atabel <atabel87@gmail.com>
2013-03-04 18:42:15 +0100

    impementation of ResourceIterator used in AbstractResourceSource

--- src/main/java/es/uah/cc/ie/metadatastatistics/AbstractResourceSource.java
+++ src/main/java/es/uah/cc/ie/metadatastatistics/AbstractResourceSource.java
@@ -1,16 +1,22 @@
 package es.uah.cc.ie.metadatastatistics;
 
 import java.util.Iterator;
+import java.util.List;
 
 public class AbstractResourceSource implements ResourceSource {
 
     private MetadataParser parser;
+    private List source;
 
     public AbstractResourceSource(MetadataParser parser) {
         this.parser = parser;
     }
 
     public Iterator<Resource> iterator() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return new ResourceIterator(this.getSource() , parser);
+    }
+    
+    protected List getSource() {
+        return this.source;
     }
 }
